94f74c87808c18f6880b5bda6ead4e8e1c47ef61
gpichot <gpichot@ebay.com>
2018-07-10 18:25:03 +0100

    Added new feature whereAmI that informs of the location

--- src/main/javascript/hello.js
+++ src/main/javascript/hello.js
@@ -1,5 +1,8 @@
 module.exports = {
   "hello": function(){
     return "Hello World";
+  },
+  "whereAmI": function(){
+    return "Welcome to FullStack London 2018";
   }
 };
--- src/test/javascript/hello-test.js
+++ src/test/javascript/hello-test.js
@@ -6,5 +6,10 @@ describe('Application', function(){
     it('should greet the world', function(){
       assert.equal("Hello World", app.hello());
     });
-  });
+  }),
+  describe('whereAmI()', function(){
+    it('should inform about the location', function(){
+      assert.equal("Welcome to FullStack London 2018", app.whereAmI());
+    });
+  })
 });
